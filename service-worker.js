62ee9cbf7e543da9b40dc0f9b8dbd9a960d8502b
Lars87-maker <larsstevens@hotmail.com>
2025-08-06 13:33:25 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "rooster-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  console.log("Service Worker: Installed & Assets Cached");
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("Service Worker: Clearing old cache:", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  console.log("Service Worker: Activated");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
